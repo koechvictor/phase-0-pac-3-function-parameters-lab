af6f92e1f099eeb746b4b8b6fdbc558ba035608e
koech victor <kipkiruiv408@gmail.com>
2024-06-21 13:28:20 +0000

    update

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function introductionWithLanguage(name, language) {
   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language = "JavasScript") {
-  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-}
-
 function introductionWithLanguageOptional(
   name = "Gracie",
   language = "JavaScript"
